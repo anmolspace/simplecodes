d98362ac9faf276c3a2ce678c891ec14bb263c36
anmolspace <98482454+anmolspace@users.noreply.github.com>
2022-10-01 14:21:15 +0530

    Updated Prime Palindrome code

--- First_n_digit_Prime_Palindrome_in_pi.py
+++ First_n_digit_Prime_Palindrome_in_pi.py
@@ -1,9 +1,10 @@
+# This code will find the first n digit prime palindrome in the decimals of pi.
 
 import sympy as s  #Use sympy to check prime because it checks faster
 files = open("C:") #Location of the txt file containing the digits of pi
 pi = files.read()
 files.close()
-def pin(n):
+def pppi(n):
   size = len(pi)
   for i in range(2, size-n):
       p = pi[i:i+n]
@@ -12,5 +13,3 @@ def pin(n):
           if s.isprime(int(p)):
             print(p)
             break
-
-pin(21)
